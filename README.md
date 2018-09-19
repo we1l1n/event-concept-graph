5f21a788c50873aacfa7e271bec2942bb3135169
phiedulxp <phiedulxp@gmail.com>
2018-09-20 07:28:53 +0800

    v2 extracted_graph/triples_from_event_tweet_v2.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # event-concept-graph
-microsoft concept graph + commonsense conceptnet5 + wikipedia current event + twitter news tweet -> event concept graph
+  - wikipedia current event
+  - twitter news tweet
+  - microsoft concept graph
+  - commonsense conceptnet5
+
+# event triple extraction demo
+![demo](./pic/demo.jpg)
--- extracted_graph/triples_from_event_tweet_v2.py
+++ extracted_graph/triples_from_event_tweet_v2.py
@@ -30,40 +30,15 @@ class StanfordCoreNLP:
         
 snlp = StanfordCoreNLP(host='10.0.1.7')
 
-from gensim.models import KeyedVectors
-from gensim.similarities.index import AnnoyIndexer
-wv = KeyedVectors.load_word2vec_format('glove.6B.300d.bin', binary=True)
-annoy_index = AnnoyIndexer()
-annoy_index.load('glove.6B.300d.index')
-annoy_index.model = wv
-vocab_disasters_and_accdients = dict(wv.most_similar(positive=['disaster','accident',], topn=25000, indexer=annoy_index))
-
-from collections import defaultdict
-from nltk.corpus import stopwords
-list_stopWords=list(set(stopwords.words('english')))
-
-NER_ = ['person','misc','money','number','ordinal','percent','date','time','duration','set','email','url']
-DEP_ = ['amod','punct','case','det','ROOT','dep','cc']
+NER_ = set(['person','misc','money','number','ordinal','percent','date','time','duration','set','email','url'])
+phrase_dict = {line.split('\t')[1].strip():float(line.split('\t')[0]) \
+               for line in codecs.open('./AutoPhrase.txt','r',encoding='utf-8').readlines()}
 
 def standard_str(string):
     return string.replace(' ','_')\
                     .replace('/','_')\
                     .replace('-','_')\
                     .replace(':','_')
-    if ' ' in string:
-        return '_'.join(string.split(' '))
-    elif '/' in string:
-        return '_'.join(string.split('/'))
-    elif '-' in string:
-        return '_'.join(string.split('-'))
-    elif ':' in string:
-        return '_'.join(string.split(':'))
-    else:
-        return string
-
-def dep_to_str(tokens_dict,entity_dict,index):
-    return entity_dict[index] if index in entity_dict.keys() \
-    else tokens_dict[index]['lemma']
 
 def annotate_sentences(text):
     out = snlp.annotate(text, properties={ 'annotators':'ner,depparse','outputFormat': 'json'})
@@ -74,46 +49,119 @@ def annotate_sentences(text):
         triples.extend(get_sentence_triples(sentence))
     return triples
     
+def get_ner_phrase(text):
+    text = text.split('_')
+    phrase = [text[-1]]
+    for i in range(2,len(text)+1):
+        if ' '.join(text[-i:]) in phrase_dict.keys() and phrase_dict[' '.join(text[-i:])]>0.1:
+            phrase.append('_'.join(text[-i:]))
+    return phrase
+
+def get_com_phrase(tokens):
+    text = text.split('_')
+    phrase = [text[-1]]
+    for i in range(2,len(text)+1):
+        #print(' '.join(text[-i:]))
+        if ' '.join(text[-i:]) in phrase_dict.keys() and phrase_dict[' '.join(text[-i:])]>0.1:
+            phrase.append('_'.join(text[-i:]))
+    return phrase
+
+def dep_rules(v=None,dep=None):
+    if dep['d'] in set(['parataxis','conj:or']):return (dep['g'],'/r/tweet/dep/para',dep['dd'])
+    if dep['d'] in set(['nmod:including']):return (dep['dd'],'/r/tweet/dep/IsA',dep['g'])
+    if dep['d'] in set(['nmod:in','nmod:on','nmod:into','nmod:at','nmod:near','nmod:under',\
+                        'nmod:off','nmod:after','nmod:across','nmod:during','nmod:through',\
+                        'nmod:following']):return (dep['g'],'/r/tweet/dep/'+dep['d'],dep['dd'])
+    if dep['gp'].startswith('N') and dep['ddp'].startswith('N'):
+        if dep['d'] in set(['nmod:by','nmod:from','nmod:due_to']):return (dep['dd'],'/r/tweet/dep/cause',dep['g'])
+    if dep['gp'].startswith('V') and dep['ddp'].startswith('V'):
+        if dep['d'] in set(['advcl:after','advcl:while','dep','acl',\
+                            'ccomp','conj','xcomp','conj:and']):return (dep['dd'],'/r/tweet/dep/after',dep['g'])
+    if dep['gp'].startswith('V') and dep['ddp'].startswith('N'):
+        try:
+            if dep['d'] in set(['dobj','nsubjpass','nmod:with','nmod:to','acl:to']):v[dep['g']]['t'].add(dep['dd'])
+            if dep['d'] in set(['nsubj','nmod:agent','nsubj:xsubj']):v[dep['g']]['s'].add(dep['dd'])
+        except:
+            import pdb;pdb.set_trace()
+
 def get_sentence_triples(sentence):
-    # tokens 
+    # token
     tokens_dict = {}
-    [tokens_dict.update({t['index']:{'lemma':standard_str(t['lemma'].lower()),'word':t['word'],'pos':t['pos']}}) for t in sentence['tokens']]
-    # entity 
-    entity = [{'text':standard_str(e['text'].lower()),'ner':e['ner'].lower(),'tokenBegin':e['tokenBegin']+1,'tokenEnd':e['tokenEnd'],}\
+    [tokens_dict.update({t['index']:{'token':[standard_str(t['lemma'].lower())],'pos':t['pos']}}) \
+     for t in sentence['tokens'] ]
+    # entity
+    entity = [{'text':standard_str(e['text'].lower()),'ner':e['ner'].lower(),\
+               'tokenBegin':e['tokenBegin']+1,'tokenEnd':e['tokenEnd'],} \
               for e in sentence['entitymentions']]
-    entity_dict = {}
-    span_entity = [] # combine in dep
-    entity_triples = []
-    for e in entity:
-        entity_dict.update({e['tokenEnd']:e['ner'] if e['ner'] in NER_ else e['text']})#[e['ner'],e['text']]})
-        if e['ner'] not in NER_:
-            entity_triples.append(('/c/en/'+e['text'],'/r/tweet/IsA','/c/en/'+e['ner']))
-            #print('/c/en/'+e['text'],'/r/tweet/IsA','/c/en/'+e['ner'])
-        if e['tokenEnd'] != e['tokenBegin']:
-            span_entity.append((e['tokenBegin'],e['tokenEnd']))
-    # dep triples
-    # vocab_disasters_and_accdients
     dep = sentence['enhancedPlusPlusDependencies']
-    dep = [{'from':d['governor'],'dep':d['dep'],'to':d['dependent']} for d in dep if d['dep'] not in DEP_ \
-       and (d['dependent'],d['governor']) not in span_entity and d['dependentGloss'] not in list_stopWords\
-      and d['governorGloss'] not in list_stopWords]# and d['dependentGloss'] in vocab_disasters_and_accdients.keys()\
-      #and d['governorGloss'] in vocab_disasters_and_accdients.keys()]
-    dep_triples =[('/c/en/'+dep_to_str(tokens_dict,entity_dict,d['to']),'/r/tweet/dep/'+d['dep'],\
-                   '/c/en/'+dep_to_str(tokens_dict,entity_dict,d['from'])) for d in dep]
-    # fact_triples filter from dep
-    fact_dict = defaultdict(dict)
+    headword = defaultdict(list)
     for d in dep:
-        if d['dep'].startswith('nsubj') or d['dep'].startswith('dobj'):
-                fact_dict[d['from']].update({d['dep']:d['to']})
+        if d['dep'] == 'compound':
+            headword[d['governor']].append(d['dependent'])   
+    entity_triples = []
+    for ent in entity:
+        if ent['ner'] in NER_:
+            if ent['tokenEnd'] in headword.keys():
+                tokens_dict[ent['tokenEnd']].update({'token':[ent['ner']],'pos':'NER'})
+                for t in headword[ent['tokenEnd']]:
+                    tokens_dict.pop(t)
+            else:
+                tokens_dict[ent['tokenEnd']].update({'token':[ent['ner']],'pos':'NER'})
+        else:
+            if ent['tokenBegin'] == ent['tokenEnd']:
+                tokens_dict[ent['tokenBegin']]['token'].append(ent['ner'])
+                entity_triples.append(('/c/en/'+tokens_dict[ent['tokenBegin']]['token'][0],'/r/tweet/IsA','/c/en/'+ent['ner']))
+                if ent['ner'] == 'cause_of_death':
+                    entity_triples.append(('/c/en/'+tokens_dict[ent['tokenBegin']]['token'][0],'/r/tweet/Cause','/c/en/death'))
+            else:
+                ner_phrases = get_ner_phrase(ent['text'])
+                for phrase in ner_phrases:
+                    if ent['ner'] == 'cause_of_death':entity_triples.append(('/c/en/'+phrase,'/r/tweet/Cause','/c/en/'+'death'))
+                    entity_triples.append(('/c/en/'+phrase,'/r/tweet/IsA','/c/en/'+ent['ner']))
+                for i in range(len(ner_phrases)):
+                    for j in range(i+1,len(ner_phrases)):
+                        entity_triples.append(('/c/en/'+ner_phrases[j],'/r/tweet/IsA','/c/en/'+ner_phrases[i]))
+                com_phrases = []
+                if ent['tokenEnd'] in headword.keys():
+                    tokens = [tokens_dict[t]['token'][0] for t in headword[ent['tokenEnd']]] + [tokens_dict[ent['tokenEnd']]['token'][0]]
+                    com_phrases = get_ner_phrase('_'.join(tokens))
+                    for t in headword[ent['tokenEnd']]:
+                        tokens_dict.pop(t)
+                phrases = set(ner_phrases+com_phrases)
+                #print(phrases)
+                tokens_dict[ent['tokenEnd']]['token'].append(ent['ner'])
+                for phrase in phrases:
+                    tokens_dict[ent['tokenEnd']]['token'].append(phrase)
+                tokens_dict[ent['tokenEnd']]['token'] = list(set(tokens_dict[ent['tokenEnd']]['token']))
+    # dep
+    tokens_dict = {k:v for k,v in tokens_dict.items() if v['pos'].startswith('N') or v['pos'].startswith('V')}
+    tokens_index_set = set(tokens_dict.keys())
+    dep = [{'d':d['dep'],'g':d['governor'],'gp':tokens_dict[d['governor']]['pos'],\
+        'dd':d['dependent'],'ddp':tokens_dict[d['dependent']]['pos']} \
+        for d in dep if d['dep'] != 'compound' and\
+       d['dependent'] in tokens_index_set and\
+       d['governor'] in tokens_index_set] 
+    V = set([d['g'] for d in dep if d['gp'].startswith('V')])
+    V_fact = {v:{'s':set(),'t':set()} for v in V}   
+    dep_triples_index = [dep_rules(v=V_fact,dep=d) for d in dep if dep_rules(v=V_fact,dep=d)]   
+    dep_triples = []
+    for dt in dep_triples_index:
+        #print(dt)
+        for s in tokens_dict[dt[0]]['token']:
+            for t in tokens_dict[dt[2]]['token']:
+                dep_triples.append(('/c/en/'+s,dt[1],'/c/en/'+t)) 
+    # fact   
     fact_triples = []
-    for k,v in fact_dict.items():
-        if 'nsubj' in v.keys() and 'dobj' in v.keys():
-            fact_triples.append(('/c/en/'+dep_to_str(tokens_dict,entity_dict,v['nsubj']),\
-                                 '/r/tweet/open/'+dep_to_str(tokens_dict,entity_dict,k),\
-                                 '/c/en/'+dep_to_str(tokens_dict,entity_dict,v['dobj'])))
-    # cat all triples
-    triples = entity_triples + dep_triples + fact_triples
-    return triples
+    for k,v in V_fact.items():
+        if len(v['s'])>0 and len(v['t'])>0:
+            for s in v['s']:
+                for t in v['t']:
+                    for ss in tokens_dict[s]['token']:
+                        for rr in tokens_dict[k]['token']:
+                            for tt in tokens_dict[t]['token']: 
+                                #print(ss,rr,tt)
+                                fact_triples.append(('/c/en/'+ss,'/r/tweet/open/'+rr,'/c/en/'+tt))
+    return entity_triples + dep_triples + fact_triples
 
 def safe_load(text):
     try:
@@ -124,7 +172,7 @@ def safe_load(text):
 def category_filter(sample):
     if not sample:
         return False
-    return sample['event']['category'].startswith('disaster')
+    return sample['event']['category'].startswith('disaster') or sample['event']['category'].startswith('army')
 
 def get_event_triples(sample):
     event_triples = []
